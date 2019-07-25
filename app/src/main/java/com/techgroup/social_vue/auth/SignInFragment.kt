a22d916ad2317429c17df093e41ea79260872f32
kingsleyusoroeno <kingsleyenou@gmail.com>
2019-07-25 16:34:24 +0100

    Included Navigation Safe-args and a user data class

--- app/src/main/java/com/techgroup/social_vue/auth/SignInFragment.kt
+++ app/src/main/java/com/techgroup/social_vue/auth/SignInFragment.kt
@@ -2,14 +2,13 @@ package com.techgroup.social_vue.auth
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import com.techgroup.social_vue.R
-import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 /**
  * A simple [Fragment] subclass.
@@ -26,8 +25,9 @@ class SignInFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
         val tvCreateAccount = view.findViewById<TextView>(R.id.tv_create_account)
 
-        tvCreateAccount.setOnClickListener {
-            it.findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        val user = arguments?.let {
+            Log.i("SignInFragment", "User Details is $it")
+            SignInFragmentArgs.fromBundle(it)
         }
 
         return view
